83a3edfb8c31df6a48ed838f67171c59d84af2e0
davidingles <davidingleslopez@gmail.com>
2024-03-05 08:44:19 +0100

    muy bien el libreto

--- src/components/VideoReactPrincipal.jsx
+++ src/components/VideoReactPrincipal.jsx
@@ -1,17 +1,17 @@
 import { useState, useEffect, useRef } from 'react';
 import estilos from './VideoReactPrincipal.module.css'
 
-
 const libreto = {
   "vCAJA_FLEXO": {
     "DESCRIPCION": "Caja típica / modelo básico conocido como B1",
-    "DESCRIPCION2":"Impresión Flexográfica",
-    "IMPRESON": 10,
+    "DESCRIPCION2": "Impresión Flexográfica",
+    "IMPRESION": 10
+    ,
   },
   "v_1L0024": {
     "DESCRIPCION": "Bandeja automontable básica",
     // "DESCRIPCION2":"Impresión Flexográfica",
-    "IMPRESON": 2,
+    "IMPRESION": 2,
   },
 }
 
@@ -19,6 +19,7 @@ const videosPrincipales = Object.keys(libreto);
 
 function VideoPlayer() {
   const [currentVideo, setCurrentVideo] = useState(Math.floor(Math.random() * videosPrincipales.length));
+  const [showDescription2, setShowDescription2] = useState(false);
   const videoRef = useRef(null);
 
   useEffect(() => {
@@ -27,7 +28,16 @@ function VideoPlayer() {
     }
   }, [currentVideo]);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowDescription2(true);
+    }, libreto[videosPrincipales[currentVideo]].IMPRESION * 1000);
+
+    return () => clearTimeout(timer);
+  }, [currentVideo]);
+
   const handleVideoEnd = () => {
+    setShowDescription2(false);
     setCurrentVideo((prevVideo) => (prevVideo + 1) % videosPrincipales.length);
   };
 
@@ -44,13 +54,19 @@ function VideoPlayer() {
       <section>
         {videosPrincipales.map((video, index) => {
           if (currentVideo === index) {
-            return <h1 key={index} className={estilos.david}>{libreto[videosPrincipales[currentVideo]].DESCRIPCION}</h1>
-          
+            return (
+              <div key={index}>
+                <h1 key={index} className={estilos.david}>{libreto[videosPrincipales[currentVideo]].DESCRIPCION}</h1>
+                {showDescription2 && <h2 className={estilos.david2}>{libreto[videosPrincipales[currentVideo]].DESCRIPCION2}</h2>}
+              </div>
+            )
+
           }
           return null;
         })}
       </section>
       <video
+        className={estilos.video2}
         muted
         autoPlay
         ref={videoRef}
