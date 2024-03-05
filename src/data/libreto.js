61c9499bce5a6aad5736abba46dcda0da4375b6d
davidingles <davidingleslopez@gmail.com>
2024-03-05 12:55:02 +0100

    cabellete vinilo---todo funciona bien

---
+++ src/data/libreto.js
@@ -0,0 +1,22 @@
+export const libreto = {
+  "vCAJA_FLEXO": {
+    "DESCRIPCION": "Caja típica / modelo básico conocido como B1",
+    "DESCRIPCION2": "Impresión Flexográfica",
+    "IMPRESION": 10
+  },
+  "v1G0011B": {
+    "DESCRIPCION": "Estuche apilable y automontable para usar en expositores unidos",
+    "DESCRIPCION2": "Impresión Flexográfica hasta 3 colores",
+    "IMPRESION": 10,
+  },
+  "v4G02870001_cazoletas": {
+    "DESCRIPCION": "Estuche dispensador",
+    // "DESCRIPCION2": "Impresión Flexográfica",
+    // "IMPRESION": 10,
+  },
+  "videoCaballeteVinilo": {
+    "DESCRIPCION": "Caballete para expositores",
+    "DESCRIPCION2": "Impresión en Vinilo Adhesivo",
+    "IMPRESION": 6,
+  },
+}
---
+++ src/data/libreto.old.js
@@ -0,0 +1,53 @@
+export const libreto = {
+  "vCAJA_FLEXO": {
+    "DESCRIPCION": "Caja típica / modelo básico conocido como B1",
+    "DESCRIPCION2": "Impresión Flexográfica",
+    "IMPRESION": 10
+    ,
+  },
+  "v_1L0024": {
+    "DESCRIPCION": "Bandeja automontable básica",
+    // "DESCRIPCION2":"Impresión Flexográfica",
+    // "IMPRESION": 2,
+  },
+  "v1P0221": {
+    "DESCRIPCION": "Estuche automontable",
+    // "DESCRIPCION2": "Impresión Offset",
+    // "IMPRESION": 5,
+  },
+  "video4e0066": {
+    "DESCRIPCION": "Estuche 3 botellas con agarrador incluído en el diseño",
+    // "DESCRIPCION2": "Impresión Offset",
+    // "IMPRESION": 7,
+  },
+  "videoJamonero": {
+    "DESCRIPCION": "Estuche Jamonero con asa de plástico",
+    // "DESCRIPCION2": "Impresión Flexográfica",
+    // "IMPRESION": 10,
+  },
+  "videoEstuche1botella": {
+    "DESCRIPCION": "Estuche para una botella con fijador en la tapa",
+    // "DESCRIPCION2": "Impresión Offset",
+    // "IMPRESION": 5,
+  },
+  "videoCajaConBotes": {
+    "DESCRIPCION": "Caja con separador incluído. Para tarros, lastas, botes...",
+    // "DESCRIPCION2": "Impresión Flexográfica",
+    // "IMPRESION": 10,
+  },
+  "videoCaballete": {
+    "DESCRIPCION": "Caballete para expositores",
+    // "DESCRIPCION2": "Impresión Flexográfica",
+    // "IMPRESION": 10,
+  },
+  "v4G02870001_cazoletas": {
+    "DESCRIPCION": "Dispensador de cazoletas",
+    // "DESCRIPCION2": "Impresión Flexográfica",
+    // "IMPRESION": 10,
+  },
+  "v1G0011B": {
+    "DESCRIPCION": "Estuche apilable y automontable para usar en expositores unidos",
+    // "DESCRIPCION2": "Impresión Flexográfica",
+    // "IMPRESION": 10,
+  },
+}
